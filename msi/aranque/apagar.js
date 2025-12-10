5fd879294eaf92e5b73bdbf082eb8a964a3517e8
Skynisys <38273297+Gnirvin@users.noreply.github.com>
2025-12-09 20:10:09 -0600

    Add files via upload

---
+++ msi/aranque/apagar.js
@@ -0,0 +1,4 @@
+// Espera unos segundos y luego redirige al sistema principal
+setTimeout(() => {
+  window.location.href = "tanke.html"; // Ajusta la ruta según estructura
+}, 5000); // 5 segundos de carga
