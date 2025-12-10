63128e678a52da56fed751df785faf67e5cf19f5
Skynisys <38273297+Gnirvin@users.noreply.github.com>
2025-12-09 20:09:10 -0600

    Add files via upload

---
+++ legacy.js
@@ -0,0 +1,5 @@
+
+// Espera unos segundos y luego redirige al sistema principal
+setTimeout(() => {
+  window.location.href = "./msi/aranque/index.html"; // Ajusta la ruta según estructura
+}, 5000); // 5 segundos de carga
